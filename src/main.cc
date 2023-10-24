6ffa7685724dce88b0a6a008fd0741c8a90c44bb
srijan mukherjee <emailofsrijan@gmail.com>
2023-10-25 01:41:33 +0530

    added argument parsing

--- src/main.cc
+++ src/main.cc
@@ -1,42 +1,125 @@
+#include <cstdlib>
 #include <cstring>
 #include <exception>
 #include <fstream>
 #include <ios>
 #include <iostream>
+#include <stdexcept>
 #include <string>
+#include <filesystem>
 
 #include "huffman.h"
+#include "../lib/argparse/include/argparse/argparse.hpp"
+
+#define EXTENSION ".bin"
+
+namespace fs = std::filesystem;
 
 int main(int argc, const char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s FILE\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+    // TODO: pass version
+    argparse::ArgumentParser program("huffman");
 
-    std::ifstream srcFile(argv[1]);
+    program.add_argument("file")
+        .help("path to file");
+    
+    program.add_argument("--verbose")
+        .help("enable verbose output")
+        .default_value(false)
+        .implicit_value(true);
 
-    if (!srcFile.is_open()) {
-        fprintf(stderr, "Couldn't open file '%s': %s\n", argv[1], strerror(errno));
-        return EXIT_FAILURE;
-    }
-
-    auto compressor = huffman::Compressor(srcFile);
-
-    std::ofstream destFile("compressed.bin", std::ios::out | std::ios::binary);
+    program.add_argument("-d", "--decompress")
+        .help("decompress file")
+        .default_value(false)
+        .implicit_value(true);
+    
+    program.add_argument("-o", "--output")
+        .default_value(std::string("")) 
+        .help("output file path");
 
     try {
-        compressor.WriteToFile(destFile);
-    } catch (std::exception e) {
-        std::cerr << e.what() << std::endl;
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        return 1;
     }
 
-    destFile.close();
-    srcFile.close();
+    std::string srcFilePath = program.get<std::string>("file");
+    std::string destFilePath = program.get<std::string>("--output");
+    bool shouldDecompress = program.get<bool>("--decompress");
 
-    std::ifstream compressedSrc("compressed.bin", std::ios::binary | std::ios::in);
-    std::ofstream uncompressedDest("uncompressed.txt", std::ios::binary | std::ios::out);
-    huffman::Decompressor decompressor(compressedSrc);
-    decompressor.WriteToFile(uncompressedDest);
+    if (!fs::exists(srcFilePath)) {
+        fprintf(stderr, "ERROR: File doesn't exist\n");
+        return EXIT_FAILURE;
+    }
+
+    if (fs::is_directory(srcFilePath)) {
+        fprintf(stderr, "ERROR: Directories are not supported yet\n");
+        return EXIT_FAILURE;
+    }
+
+    if (fs::is_symlink(srcFilePath)) {
+        fprintf(stderr, "ERROR: Symlinks are not supported yet\n");
+        return EXIT_FAILURE;
+    }
+
+    if (destFilePath == "") {
+        auto path = fs::path(srcFilePath);
+
+        if (shouldDecompress) {
+            if (path.extension() == EXTENSION) {
+                path = path.replace_extension("");
+            }
+            destFilePath = path.filename();
+        } else {
+            destFilePath = fs::path(srcFilePath).filename();
+            destFilePath = destFilePath + EXTENSION;
+        }
+    }
+
+    else {
+        auto path = fs::path(destFilePath);
+
+        if (fs::is_directory(path)) {
+            destFilePath = fs::path(srcFilePath).filename();
+            destFilePath = destFilePath + EXTENSION;
+            path.append(destFilePath);
+            destFilePath = path.string();
+        }
+    }
+
+    std::ifstream srcFile(srcFilePath, std::ios::in | std::ios::binary);
+    std::ofstream destFile(destFilePath, std::ios::out | std::ios::binary);
+
+    if (!srcFile.is_open()) {
+        fprintf(stderr, "Couldn't open file '%s': %s\n", srcFilePath.c_str(), strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    if (!destFile.is_open()) {
+        fprintf(stderr, "Couldn't open file '%s': %s\n", destFilePath.c_str(), strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    if (shouldDecompress) {
+        huffman::Decompressor decompressor(srcFile);
+        try {
+            decompressor.WriteToFile(destFile);
+        } catch (std::exception e) {
+            std::cerr << e.what() << std::endl;
+        }
+    } else {
+        huffman::Compressor compressor(srcFile);
+        try {
+            compressor.WriteToFile(destFile);
+        } catch (std::exception e) {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+
+    srcFile.close();
+    destFile.close();
 
     return 0;
 }
